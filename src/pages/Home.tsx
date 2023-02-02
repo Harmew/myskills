c5a9ab1fb05cb73b50e8177f6888d8b1322bb6b9
Daniel <dann.ew@outlook.com>
2023-02-01 21:31:44 -0300

    initial commit but finalized

---
+++ src/pages/Home.tsx
@@ -0,0 +1,103 @@
+import React, { useState, useEffect } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  Platform,
+  FlatList,
+} from "react-native";
+
+// Components
+import { Button } from "../components/Button";
+import { SkillCard } from "../components/SkillCard";
+
+interface SkillData {
+  id: string;
+  name: string;
+}
+
+export function Home(): JSX.Element {
+  const [newSkill, setNewSkill] = useState("");
+  const [mySkills, setMySkills] = useState<SkillData[]>([]);
+  const [greeting, setGreeting] = useState("");
+
+  function handleAddNewSkill(): void {
+    if (newSkill.trim() === "") return;
+
+    const data: SkillData = {
+      id: String(new Date().getTime()),
+      name: newSkill.trim(),
+    };
+    setMySkills((oldState) => [...oldState, data]);
+  }
+
+  function handleRemoveSkill(id: string): void {
+    setMySkills((oldState) => oldState.filter((skill) => skill.id !== id));
+  }
+
+  useEffect(() => {
+    const currentHour = new Date().getHours();
+
+    if (currentHour < 12) setGreeting("Good morning");
+    else if (currentHour >= 12 && currentHour < 18)
+      setGreeting("Good afternoon");
+    else setGreeting("Good night");
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Welcome, Daniel</Text>
+
+      <Text style={styles.greetings}>{greeting}</Text>
+
+      <TextInput
+        style={styles.input}
+        placeholder="New skill"
+        placeholderTextColor="#555"
+        onChangeText={setNewSkill}
+      />
+
+      <Button title="Add" onPress={handleAddNewSkill} />
+
+      <Text style={[styles.title, { marginVertical: 50 }]}>My Skills</Text>
+
+      <FlatList
+        data={mySkills}
+        keyExtractor={(item) => item.id}
+        showsVerticalScrollIndicator={false}
+        renderItem={({ item }) => (
+          <SkillCard
+            skill={item.name}
+            onPress={() => handleRemoveSkill(item.id)}
+          />
+        )}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#121015",
+    paddingHorizontal: 30,
+    paddingVertical: 70,
+  },
+  title: {
+    color: "#fff",
+    fontSize: 24,
+    fontWeight: "bold",
+  },
+  input: {
+    backgroundColor: "#1f1e25",
+    color: "#fff",
+    fontSize: 18,
+    padding: Platform.OS === "ios" ? 15 : 10,
+    marginTop: 30,
+    borderRadius: 7,
+  },
+  greetings: {
+    color: "#fff",
+  },
+});
